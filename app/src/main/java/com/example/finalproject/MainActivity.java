c1a166ba825d5ebcc272bea937a5cff11580f023
Lê Thành Vinh <lethanhvinhspk@gmail.com>
2024-03-05 21:34:48 +0700

    welcome
    anh em xem thử

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -2,15 +2,22 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnWelcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // This is a comment
+        btnWelcome = findViewById(R.id.btnWelcome);
+        btnWelcome.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, WelcomeActivity.class);
+            startActivity(intent);
+        });
     }
 }
---
+++ app/src/main/java/com/example/finalproject/WelcomeActivity.java
@@ -0,0 +1,45 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class WelcomeActivity extends AppCompatActivity {
+    Button btnLogin, btnSignup, btnForgotPassword;
+    EditText etUsername, etPassword;
+    TextView tvWelcomeTitle;
+    Animation animation_btn_bottom, animation_et_right, animation_et_left, animation_tvHomeTitle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        // Set up Animation
+        btnLogin = findViewById(R.id.btnLogin);
+        animation_btn_bottom = AnimationUtils.loadAnimation(this, R.anim.anime_btn_bottom);
+        btnLogin.setAnimation(animation_btn_bottom);
+
+        btnSignup = findViewById(R.id.btnSignup);
+        btnSignup.setAnimation(animation_btn_bottom);
+
+        btnForgotPassword = findViewById(R.id.btnForgot);
+        btnForgotPassword.setAnimation(animation_btn_bottom);
+
+        etUsername = findViewById(R.id.et_username);
+        animation_et_right = AnimationUtils.loadAnimation(this, R.anim.anime_et_right);
+        etUsername.setAnimation(animation_et_right);
+
+        etPassword = findViewById(R.id.et_password);
+        animation_et_left = AnimationUtils.loadAnimation(this, R.anim.anime_et_left);
+        etPassword.setAnimation(animation_et_left);
+
+        tvWelcomeTitle = findViewById(R.id.tvWelcomeTitle);
+        animation_tvHomeTitle = AnimationUtils.loadAnimation(this, R.anim.anime_tv);
+        tvWelcomeTitle.setAnimation(animation_tvHomeTitle);
+    }
+}
