9647ca5696127fa84392e27192878d2a9c2ab158
thanhhung0203 <hungat418@gmail.com>
2024-03-07 15:53:49 +0700

    Bổ sung icon và chức năng  ẩn hiện mật khẩu ở pass và re-enter pass

--- app/src/main/java/com/example/finalproject/SignUpActivity.java
+++ app/src/main/java/com/example/finalproject/SignUpActivity.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.view.animation.Animation;
 import android.widget.Button;
@@ -44,5 +48,33 @@ public class SignUpActivity extends AppCompatActivity {
         edtRepassword = findViewById(R.id.edtRepassword);
         animation_et_left = AnimationUtils.loadAnimation(this,R.anim.anime_et_left);
         edtRepassword.setAnimation(animation_et_left);
+
+        ShowPass(edtPassword);
+        ShowPass(edtRepassword);
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    public void ShowPass(EditText pass){
+        pass.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                final int DRAWABLE_RIGHT = 2;
+
+                if(event.getAction() == MotionEvent.ACTION_UP) {
+                    if(event.getRawX() >= (pass.getRight() - pass.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                        if (pass.getTransformationMethod() == HideReturnsTransformationMethod.getInstance()) {
+                            pass.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                            pass.setCompoundDrawablesWithIntrinsicBounds(R.drawable.wel_password, 0, R.drawable.wel_eyes, 0);
+                        } else {
+                            pass.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                            pass.setCompoundDrawablesWithIntrinsicBounds(R.drawable.wel_password, 0, R.drawable.wel_eyes_close, 0);
+                        }
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+
     }
 }
