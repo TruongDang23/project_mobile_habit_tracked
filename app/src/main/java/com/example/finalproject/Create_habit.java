633064c1368ed96ec24d0b9d15334e519ee17929
BaoVu0712 <91738440+BaoVu0712@users.noreply.github.com>
2024-03-08 22:03:58 +0700

    fix create habit
    fix clock picker,date picker

--- app/src/main/java/com/example/finalproject/Create_habit.java
+++ app/src/main/java/com/example/finalproject/Create_habit.java
@@ -2,13 +2,21 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
 
-public class Create_habit extends AppCompatActivity {
+import java.util.Calendar;
+import java.util.Locale;
+
+public class Create_habit extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_habit);
+
     }
 }
