7458b3952ee3c5cda11a9372ffc96742ad428a10
Lê Thành Vinh <lethanhvinhspk@gmail.com>
2024-03-11 08:41:40 +0700

    fix some ui

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnWelcome, btnHomeMain;
+    Button btnWelcome, btnHomeMain, btnSettings, btnCreateHabit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +27,16 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
+        btnSettings = findViewById(R.id.btnSettings);
+        btnSettings.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, Setting.class);
+            startActivity(intent);
+        });
+
+        btnCreateHabit = findViewById(R.id.btnCreateHabit);
+        btnCreateHabit.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, Create_habit.class);
+            startActivity(intent);
+        });
     }
 }
