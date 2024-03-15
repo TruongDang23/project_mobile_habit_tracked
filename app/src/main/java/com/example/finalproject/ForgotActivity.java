b9af02673f3da237c37e4de3aba9bf036c170d54
QuocNguyen332003 <123507130+QuocNguyen332003@users.noreply.github.com>
2024-03-15 22:36:03 +0700

    Forgot

--- app/src/main/java/com/example/finalproject/ForgotActivity.java
+++ app/src/main/java/com/example/finalproject/ForgotActivity.java
@@ -1,20 +1,45 @@
 package com.example.finalproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.finalproject.modal.Account;
+import com.example.finalproject.modal.GmailSender;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class ForgotActivity extends AppCompatActivity {
-    Button btnGetpass;
+
+    private FirebaseDatabase database;
+    private DatabaseReference ref;
+    Button btnGetPass;
     ImageButton imgLogin;
+    GmailSender Gmail = new GmailSender();
     EditText edt_username, edt_email, edt_yourpassword;
+
     TextView tvWelcomeTitle;
     Animation animation_et_right, animation_et_left, animation_tvHomeTitle;
     @Override
@@ -43,5 +68,54 @@ public class ForgotActivity extends AppCompatActivity {
         edt_yourpassword = findViewById(R.id.edtYourpassword);
         animation_et_right = AnimationUtils.loadAnimation(this,R.anim.anime_et_right);
         edt_yourpassword.setAnimation(animation_et_right);
+
+        btnGetPass = findViewById(R.id.btnGetpass);
+        btnGetPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getPass();
+                Intent intent = new Intent(ForgotActivity.this, WelcomeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    public void getPass(){
+        database = FirebaseDatabase.getInstance();
+        ref = database.getReference("Habit_Tracker").child("Tai_Khoan");
+
+        final String username = edt_username.getText().toString();
+        final String gmail = edt_email.getText().toString();
+
+        if (TextUtils.isEmpty(username) || TextUtils.isEmpty(gmail)) {
+            Toast.makeText(this, "Vui lòng nhập tên đăng nhập và gmail", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot taiKhoanSnapshot : dataSnapshot.getChildren()) {
+                        String currentUsername = taiKhoanSnapshot.child("TenDangNhap").getValue(String.class);
+                        String currentGmail = taiKhoanSnapshot.child("Gmail").getValue(String.class);
+                        if (username.equals(currentUsername) && gmail.equals(currentGmail)) {
+                            String currentPassword = taiKhoanSnapshot.child("MatKhau").getValue(String.class);
+                            String senderEmail = "01215165330asd@gmail.com"; // Địa chỉ Gmail của bạn
+                            String senderPassword = "uwcpdnjcsxhchtcv"; // Mật khẩu Gmail của bạn
+                            String recipientEmail = gmail; // Địa chỉ Gmail của người nhận
+                            String subject = "Gửi lại mật khẩu";
+                            String message = "Password của bạn: " + currentPassword;
+                            Gmail.sendEmail(senderEmail, senderPassword, recipientEmail, subject, message);
+                            return;
+                        }
+                    }
+                    Toast.makeText(ForgotActivity.this, "Tài khoản và  gmail không khớp với nhau", Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Xử lý lỗi khi đọc dữ liệu từ Firebase
+                Toast.makeText(ForgotActivity.this, "Lỗi khi đọc dữ liệu từ Firebase", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
