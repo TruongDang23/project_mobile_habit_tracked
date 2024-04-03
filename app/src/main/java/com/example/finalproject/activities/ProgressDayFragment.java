e321cfe0bf4c8cde2dc30eef98e058ceac2f31ca
ThanhHung0203 <hungat418@gmail.com>
2024-04-03 12:43:04 +0700

    xong co ban back_end_progress_detail_day

--- app/src/main/java/com/example/finalproject/activities/ProgressDayFragment.java
+++ app/src/main/java/com/example/finalproject/activities/ProgressDayFragment.java
@@ -2,6 +2,7 @@ package com.example.finalproject.activities;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -15,10 +16,22 @@ import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import android.graphics.Color;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -36,6 +49,10 @@ public class ProgressDayFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private FirebaseDatabase dataBase;
+    private DatabaseReference ref;
+    private TextView tvTodayProgress;
+    private TextView tvCurrentProgress;
 
     public ProgressDayFragment() {
         // Required empty public constructor
@@ -85,17 +102,66 @@ public class ProgressDayFragment extends Fragment {
         // Lấy dữ liệu
         String idHabit = getArguments().getString("idHabit");
         String idTaiKhoan = getArguments().getString("idTaiKhoan");
-        Log.d("idHabit d", idHabit);
-        Log.d("idTaiKhoan d", idTaiKhoan);
+
         pieChart = view.findViewById(R.id.pieChart);
+        tvTodayProgress = view.findViewById(R.id.tvTodayProgress);
+        tvCurrentProgress = view.findViewById(R.id.tvCurrentProgress);
+
         setUpPieChart();
-        loadPieChartData();
+        getProgressDay(idTaiKhoan,idHabit);
 
         // Inflate the layout for this fragment
         return view;
     }
+    private void getConnection(String idTaiKhoan, String idHabit){
+        dataBase = FirebaseDatabase.getInstance();
+        ref = dataBase.getReference("Habit_Tracker").child("Du_Lieu").child(idTaiKhoan).child(idHabit);
+    }
+    private void getProgressDay(String idTaiKhoan, String idHabit){
+        getConnection(idTaiKhoan,idHabit);
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    try {
+                        double muctieu = dataSnapshot.child("MucTieu").getValue(Double.class);
+                        String donvi = dataSnapshot.child("DonVi").getValue(String.class);
+                        tvTodayProgress.setText("Target "+muctieu+" "+donvi);
+
+                        // Tính tổng khối lượng công việc đã thực hiện trong ngày
+                        double tongKhoiLuong = 0;
+                        for (DataSnapshot snapshot : dataSnapshot.child("ThoiGianThucHien").getChildren()) {
+                            String thoiGian = snapshot.getKey();
+                            SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy hh:mm:ssa");
+                            Date date = sdf.parse(thoiGian);
+                            Calendar cal = Calendar.getInstance();
+                            cal.setTime(date);
+                            Calendar today = Calendar.getInstance();
+                            if (cal.get(Calendar.YEAR) == today.get(Calendar.YEAR) &&
+                                    cal.get(Calendar.DAY_OF_YEAR) == today.get(Calendar.DAY_OF_YEAR)) {
+                                tongKhoiLuong += snapshot.getValue(Double.class);
+                            }
+                        }
+
+                        tvCurrentProgress.setText(String.valueOf(tongKhoiLuong)+" "+donvi);
+                        loadPieChartData(tongKhoiLuong,muctieu);
+                        pieChart.invalidate();
+                    }catch (Exception e){
+                        Log.d("Error", e.getMessage());
+                    }
+                }
+                else
+                    Toast.makeText(getContext(), "Không tìm thấy dữ liệu", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getContext(), "Lỗi khi đọc dữ liệu từ Firebase", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
     private void setUpPieChart() {
-        pieChart.setUsePercentValues(true);
+        pieChart.setUsePercentValues(false);
         pieChart.getDescription().setEnabled(false);
         pieChart.setExtraOffsets(5, 10, 5, 5);
         pieChart.setDragDecelerationFrictionCoef(0.95f);
@@ -109,18 +175,19 @@ public class ProgressDayFragment extends Fragment {
         l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
         l.setOrientation(Legend.LegendOrientation.VERTICAL);
         l.setDrawInside(false);
-        l.setEnabled(true);
+        l.setEnabled(false);
     }
-    private void loadPieChartData() {
+    private void loadPieChartData(double tongKhoiLuong, double muctieu) {
         // creating data values
         ArrayList<PieEntry> entries = new ArrayList<>();
-        entries.add(new PieEntry(70.5f, ""));
-        entries.add(new PieEntry(29.5f, ""));
+        entries.add(new PieEntry((float) (tongKhoiLuong / muctieu * 100), ""));
+        entries.add(new PieEntry((float) (100 - tongKhoiLuong / muctieu * 100), ""));
 
         // creating dataset
         PieDataSet dataset = new PieDataSet(entries, "");
         dataset.setSliceSpace(3f);
         dataset.setSelectionShift(5f);
+        dataset.setDrawValues(false);
 //        dataset.setColors(ColorTemplate.JOYFUL_COLORS);
         int [] colors = {R.color.Blue, R.color.white};
         dataset.setColors(colors, getContext());
